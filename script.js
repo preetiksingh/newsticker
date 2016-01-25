ef672a989378b1814e8556f795452e3d5ddd387d
preetiksingh <preetisingh41451@gmail.com>
2016-01-25 18:01:27 +0530

    Added stopTicker and startTicker functions

---
+++ script.js
@@ -0,0 +1,23 @@
+var interval;
+function startTicker()
+{
+
+    $("#news li:first").slideUp(function() {
+       $(this).appendTo($("#news")).slideDown();
+    });
+}
+
+function stopTicker()
+{
+  clearInterval(interval);
+}
+
+$(document).ready(function(){
+  interval=setInterval(startTicker, 3000);
+  $("#news").hover(function(){
+    stopTicker();
+  },function(){
+      interval = setInterval(startTicker, 3000);
+      });
+  
+});
